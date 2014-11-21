807a09ec28e53b9e0fa1c8c2f7e16a5036703b12
0xack13 <0xack13@gmail.com>
2014-11-21 18:08:36 +0300

    Change drag/drop style

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    connect: {
+      server: {
+        options: {
+          port: 9001,
+          bases: '',
+          keepalive: true
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  // Default task(s).
+  grunt.registerTask('default', ['connect']);
+
+};
